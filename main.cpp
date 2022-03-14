cfa367e1c4fe0f8b400c5bdcef30c8e86205d5b1
Herbov Heorhii <heorhii.herbov@solarwinds.com>
2022-03-14 14:28:47 +0200

    Conan and protobuf added to the project; Product interface implemented;

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <memory>
+#include <string>
+
+#include "Product.pb.h"
+
+using namespace std;
+
+const string version = "0.0.1";
 
 int main() {
-    std::cout << "Service is running, enter A to stop\n";
+    product_info::Product product;
+    product.set_version(version);
+    cout << "Service is running, enter A to stop\n";
     char ch;
     while(true) {
         ch  = getchar();
